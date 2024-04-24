eddc9daf8a970b50f6bd28b404343439448593b2
Artur <arturimankulov906@gmail.com>
2024-04-24 20:24:42 +0500

    create telegram version

---
+++ src/main/java/io/proj3ct/SpringNaumenBot/SpringNaumenBotApplication.java
@@ -0,0 +1,13 @@
+package io.proj3ct.SpringNaumenBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNaumenBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNaumenBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/proj3ct/SpringNaumenBot/SpringNaumenBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.proj3ct.SpringNaumenBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNaumenBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
