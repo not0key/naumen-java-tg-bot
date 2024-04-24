c5e2d045ab6083519094b9f7b07f8249760a8f77
Artur <arturimankulov906@gmail.com>
2024-04-24 19:53:12 +0500

    initial commit

---
+++ src/main/java/io/proj3ct/SpingNaumenBot/SpingNaumenBotApplication.java
@@ -0,0 +1,13 @@
+package io.proj3ct.SpingNaumenBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingNaumenBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingNaumenBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/proj3ct/SpingNaumenBot/SpingNaumenBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.proj3ct.SpingNaumenBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingNaumenBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
